ccc0d0cfe1f4392c465657f63ccdf17305a56ed4
schneck <schneck@3a50662e-2030-11df-98cf-3116c4ba2102>
2009-12-16 03:49:22 +0000

    Getting ready for 1.1.  Make visualization library Java-1.5-compatible.  Minor javascript improvements.

--- localweb/local.sh
+++ localweb/local.sh
@@ -2,10 +2,14 @@
 
 export where=test
 
+mkdir -p build
 sed -e '/CHANGE URL HERE/ s/localhost:8081/tupelo-schneck.org:8081/' \
-    -e '/<!--INSERT PERSONAL TEXT HERE-->/ rlocalweb/local.html' web/its-electric.html \
+    -e '/<!--INSERT PERSONAL TEXT HERE-->/ rlocalweb/local.html' \
+    -e '/>v1.1</ s/v1.1/<a href="NEWS.txt">v1.1<\/a>/' \
+    web/its-electric.html \
     > build/its-electric.html
 cp web/* /Library/WebServer/Documents/$where
+cp NEWS.txt /Library/WebServer/Documents/$where
 cp build/its-electric.html /Library/WebServer/Documents/$where
 ant
 cp build/its-electric-*.zip /Library/WebServer/Documents/$where
