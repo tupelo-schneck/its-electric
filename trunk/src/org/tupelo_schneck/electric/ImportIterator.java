6067b36e3bf2e2e539a3e78400d7f520cb463020
schneck <schneck@3a50662e-2030-11df-98cf-3116c4ba2102>
2010-04-20 16:20:56 +0000

    Change ImportIterator to use raw import rather than XML.

--- trunk/src/org/tupelo_schneck/electric/ImportIterator.java
+++ trunk/src/org/tupelo_schneck/electric/ImportIterator.java
@@ -21,32 +21,29 @@ If not, see <http://www.gnu.org/licenses/>.
 
 package org.tupelo_schneck.electric;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PushbackReader;
 import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 
-public class ImportIterator implements Iterator<Triple> {
-    static final DateFormat tedDateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+import org.apache.commons.codec.binary.Base64;
 
+public class ImportIterator implements Iterator<Triple> {
     InputStream urlStream;
-    PushbackReader reader;
+    GregorianCalendar cal = new GregorianCalendar();
+    Base64 base64 = new Base64();
     byte mtu;
+    byte[] line = new byte[25];
     volatile boolean closed;
 
     public ImportIterator(final String gatewayURL, final byte mtu, final int count) throws IOException {
         this.mtu = mtu;
         URL url;
         try {
-            url = new URL(gatewayURL+"/history/secondhistory.xml?INDEX=0&MTU="+mtu+"&COUNT="+count);
+            url = new URL(gatewayURL+"/history/rawsecondhistory.raw?INDEX=1&MTU="+mtu+"&COUNT="+count);
         }
         catch (MalformedURLException e) {
             throw new RuntimeException(e);
@@ -56,124 +53,52 @@ public class ImportIterator implements Iterator<Triple> {
         urlConnection.setReadTimeout(60000);
         urlConnection.connect();
         urlStream = urlConnection.getInputStream();
-        reader = new PushbackReader(new BufferedReader(new InputStreamReader(urlStream)),10);
         urlConnection.setReadTimeout(1000);
-        skipAhead();
+        getNextLine();
     }
 
     public void close() {
         closed = true;
         if(urlStream!=null) try { urlStream.close(); } catch (Exception e) { e.printStackTrace(); }
-        if(reader!=null) try { reader.close(); } catch (Exception e) { e.printStackTrace(); }
     }
 
-    boolean eof() throws IOException {
-        int ch = reader.read();
-        if(ch<0) return true;
-        reader.unread(ch);
-        return false;
-    }
-
-    boolean lookingAt(char ch) throws IOException {
-        int next = reader.read();
-        if(next<0) return false;
-        reader.unread(next);
-        return next == ch;
-    }
-
-    void skipWhitespace() throws IOException {
-        while(true) {
-            int ch = reader.read();
-            if(ch<0) return;
-            if(!Character.isWhitespace(ch)) {
-                reader.unread(ch);
-                return;
-            }
-        }
-    }
-
-    void skipString(String s) throws IOException {
-        for(int i = 0; i < s.length(); i++) {
-            if(lookingAt(s.charAt(i))) reader.read();
-            else {
-                for(int j = i-1; j >= 0; j--) {
-                    reader.unread(s.charAt(j));
+    public void getNextLine() {
+        try {
+            int n = 0;
+            while(n < 25) {
+                int r = urlStream.read(line,n,25-n);
+                if(r <= 0) {
+                    close();
+                    return;
                 }
-                return;
+                n += r;
             }
+            if(line[24]!='\n') close();
+        }
+        catch(IOException e) {
+            close();
         }
     }
-
-    void skipAhead() throws IOException {
-        skipWhitespace();
-        if(!lookingAt('<')) return;
-        skipString("</POWER>");
-        skipString("</DATE>");
-        while(!eof() && !lookingAt('P') && !lookingAt('D')) {
-            while(!eof() && !lookingAt('P') && !lookingAt('D')) {
-                reader.read();
-            }
-            skipString("D>");
-        }
-        skipString("POWER>");
-        skipString("DATE>");
-    }
-
-    String nextString() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        while(!eof() && !lookingAt('<')) {
-            sb.append((char)reader.read());
-        }
-        return sb.toString();
-    }
-
+    
     @Override
     public void remove() {
         throw new UnsupportedOperationException();
     }
+    
     @Override
     public boolean hasNext() {
-        if(closed) return false;
-        boolean res;
-        try {
-            skipAhead();
-            res = !eof();
-        }
-        catch(SocketTimeoutException e) {
-            res = false;
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-            res = false;
-        }
-        if(!res) {
-            close();
-        }
-        return res;
+        return !closed;
     }
+
     @Override
     public Triple next() {
         if(closed) return null;
-        try {
-            skipAhead();
-            String dateString = nextString();
-            skipAhead();
-            int power = Integer.parseInt(nextString());
-            int timestamp = (int)(ImportIterator.tedDateFormat.parse(dateString).getTime()/1000);
-//            long diff = (System.currentTimeMillis() - 1000L * res.timestamp) / 1000;
-//            // skip ridiculous values
-//            if(Math.abs(diff) > 86400 || Math.abs(res.power) > 240 * 800) {
-//                return next();
-//            }
-            return new Triple(timestamp,mtu,power);
-        }
-        catch(SocketTimeoutException e) {
-            close();
-            return null;
-        }
-        catch(Exception e) {
-            close();
-            throw new RuntimeException(e);
-        }
-    }
+        byte[] decoded = base64.decode(line);
+        if(decoded==null || decoded.length<10) return null;
+        cal.set(2000+(0x00FF & decoded[0]), decoded[1]-1, decoded[2], decoded[3], decoded[4], decoded[5]);
+        int power = TimeSeriesDatabase.intOfBytes(decoded,6);
+        Triple res = new Triple((int)(cal.getTimeInMillis() / 1000),mtu,power);
+        getNextLine();
+        return res;
+   }
 }
