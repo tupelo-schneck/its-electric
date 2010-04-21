6a84b2b3cbca5f8c1af479db2e547cfc62c488bf
schneck <schneck@3a50662e-2030-11df-98cf-3116c4ba2102>
2010-04-21 15:53:09 +0000

    Trying to have good interrupt hygiene.

--- trunk/src/org/tupelo_schneck/electric/Main.java
+++ trunk/src/org/tupelo_schneck/electric/Main.java
@@ -393,6 +393,7 @@ public class Main {
                             newDataLock.wait();
                         }
                         catch(InterruptedException e) {
+                            Thread.currentThread().interrupt();
                         }
                     }
                 }
