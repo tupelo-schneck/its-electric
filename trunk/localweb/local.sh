ee54c5498d7b4f877aa393ac8142beaf19260f15
schneck <schneck@3a50662e-2030-11df-98cf-3116c4ba2102>
2010-06-21 02:45:57 +0000

    Real-time update is slowed down to current resolution.  New Jetty version.  Changed localweb and web for coming 1.5 release.

--- trunk/localweb/local.sh
+++ trunk/localweb/local.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-export where=test
+export where=test3
 
 if false
-then export where=.; rm -rf main/* build/*
+then export where=its-electric; rm -rf main/* build/*
 fi
 
-mkdir -p build
+mkdir -p build/web
 sed -e '/CHANGE URL HERE/ s/localhost:8081/tupelo-schneck.org:8081/' \
     -e '/<!--INSERT PERSONAL TEXT HERE-->/ rlocalweb/local.html' \
-    -e '/>v1.4.2</ s/v1.4.2/<a href="NEWS.txt">v1.4.2<\/a>/' \
+    -e '/>v1.5</ s/v1.5/<a href="NEWS.txt">v1.5<\/a>/' \
     web/its-electric.html \
-    > build/its-electric.html
+    > build/web/its-electric.html
+sed -e '/CHANGE URL HERE/ s/localhost:8081/tupelo-schneck.org:8081/' \
+    -e "/<\/body>/ i\\
+    <script type='text/javascript'>var val = document.getElementById('wmax').value;if(!val || val=='')document.getElementById('wmax').value = 10000;</script>" \
+    web/its-electric-full-screen.html \
+    > build/web/its-electric-full-screen.html
+sed -e '/CHANGE URL HERE/ s/localhost:8081/tupelo-schneck.org:8081/' \
+    web/its-electric-history.html \
+    > build/web/its-electric-history.html
 cp web/* /Library/WebServer/Documents/$where
 cp NEWS.txt /Library/WebServer/Documents/$where
-cp build/its-electric.html /Library/WebServer/Documents/$where
+cp build/web/* /Library/WebServer/Documents/$where
 ant
 cp build/its-electric-*.zip /Library/WebServer/Documents/$where
