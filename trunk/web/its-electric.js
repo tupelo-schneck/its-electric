3045ffb0f15adb37f842c1510bd296dd5e175ccf
schneck <schneck@3a50662e-2030-11df-98cf-3116c4ba2102>
2010-06-26 02:08:33 +0000

    Last(?) fixes for 1.5: Servlet shouldn't send null values; better use of min and max values to set range in js

--- trunk/web/its-electric.js
+++ trunk/web/its-electric.js
@@ -186,31 +186,30 @@ ItsElectric.prototype.handleQueryResponse = function(response) {
     
     var realTimeNeedsAdjust = this.realTime && this.range && this.range.end.getTime() == this.maximum;
 
-    var rangeStart = data.getValue(0,0).getTime();
-    this.minimum = parseInt(data.getTableProperty('minimum'))*1000;
+    var numRows = data.getNumberOfRows();
+
+    var rangeStart = numRows==0 ? 0 : data.getValue(0,0).getTime();
     if(isNaN(this.minimum) || this.minimum==0) {
         this.minimum = rangeStart;
     }
-    var rangeEnd = data.getValue(data.getNumberOfRows()-1,0).getTime()
+    var rangeEnd = numRows==0 ? 0 : data.getValue(numRows-1,0).getTime()
     this.maximum = parseInt(data.getTableProperty('maximum'))*1000;
     if(isNaN(this.maximum) || this.maximum==0) {
         this.maximum = rangeEnd;
     }
 
-    if(this.partialRange) {
-        if(this.minimum < rangeStart) {
-            data.insertRows(0,1);
-            data.setValue(0,0,new Date(this.minimum));
-            for(var i = 1; i < data.getNumberOfColumns(); i++) {
-                data.setValue(0,i,0);
-            }
+    if(this.minimum!=0 && (numRows==0 || this.minimum < rangeStart)) {
+        data.insertRows(0,1);
+        data.setValue(0,0,new Date(this.minimum));
+        for(var i = 1; i < data.getNumberOfColumns(); i++) {
+            data.setValue(0,i,numRows==0 ? 0 : data.getValue(1,i));
         }
-        if(this.maximum > rangeEnd) {
-            var newRow = data.addRow();
-            data.setValue(newRow,0,new Date(this.maximum));
-            for(var i = 1; i < data.getNumberOfColumns(); i++) {
-                data.setValue(newRow,i,0);
-            }
+    }
+    if(this.maximum!=0 && (numRows==0 || this.maximum > rangeEnd)) {
+        var newRow = data.addRow();
+        data.setValue(newRow,0,new Date(this.maximum));
+        for(var i = 1; i < data.getNumberOfColumns(); i++) {
+            data.setValue(newRow,i,numRows==0 ? 0 : data.getValue(newRow-1,i));
         }
     }
 
