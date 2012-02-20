572b7715024165d587fe5d467af3c1336f9dc4d7
schneck <schneck@3a50662e-2030-11df-98cf-3116c4ba2102>
2012-02-20 03:13:01 +0000

    Copyright messages and year

--- src/org/tupelo_schneck/electric/current_cost/CurrentCostImporter.java
+++ src/org/tupelo_schneck/electric/current_cost/CurrentCostImporter.java
@@ -1,3 +1,24 @@
+/*
+This file is part of
+"it's electric": software for storing and viewing home energy monitoring data
+Copyright (C) 2009--2012 Robert R. Tupelo-Schneck <schneck@gmail.com>
+http://tupelo-schneck.org/its-electric
+
+"it's electric" is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+"it's electric" is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with "it's electric", as legal/COPYING-agpl.txt.
+If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.tupelo_schneck.electric.current_cost;
 
 import java.io.IOException;
